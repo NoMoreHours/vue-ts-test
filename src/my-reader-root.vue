53374ce2f6680ec985a35b57fa91d6def46d3ca3
David Ngugi <ndavidngugi@gmail.com>
2020-09-14 16:38:33 +0300

    Add TypeScript support and minor template changes

--- src/my-reader-root.vue
+++ src/my-reader-root.vue
@@ -8,15 +8,15 @@
         <div class="text-gray-500">
           Status: <span class="font-bold text-gray-800">{{ readingStatus }}</span>
         </div>
-        <div v-if="toRead.length > 0">
+        <div v-if="unreadArticles.length > 0">
           <h2>Reading list:</h2>
-          <div v-for="(article, index) in toRead" :key="index" class="bg-gray-200 px-4 py-2 my-2 rounded shadow">
-              <div @mouseenter="func1(article)" @mouseleave="func2(article)">
+          <div v-for="(article, index) in unreadArticles" :key="index" class="bg-gray-200 px-4 py-2 my-2 rounded shadow">
+              <div @mouseenter="showAbstract(article)">
                 <div class="flex">
                     <span class="text-grey-600">Name:</span> <span class="font-bold text-gray-900">{{ article.title}} - <a :href="article.url">Link</a></span>
                 </div>
                 <div>
-                    <button class="bg-red-400 text-white py-1 px-3 rounded-full font-bold shadow my-3" @click="func3(article)">Read it</button>
+                    <button class="bg-red-400 text-white py-1 px-3 rounded-full font-bold shadow my-3" @click="markAsRead(article)">Read it</button>
                     <a class="bg-red-400 text-white py-1 px-3 rounded-full font-bold shadow my-3" target="_" :href="article.url">Open</a>
                 </div>
                 <div v-if="func5(article)"   class="bg-gray-400 p-4">
@@ -25,7 +25,7 @@
               </div>
           </div> 
         </div>
-        <button class="bg-red-400 text-white py-1 px-3 rounded-full font-bold shadow my-3 " @click.prevent="func6">Add reading</button>
+        <button class="bg-red-400 text-white py-1 px-3 rounded-full font-bold shadow my-3 " @click.prevent="fetchArticle">Add reading</button>
       </div>
   </div>
   </div>
@@ -33,75 +33,76 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
+ import Vue from "vue";
+ import Component from "vue-class-component";
 
-export default {
-  name: 'App',
-  data() {
-    return {
-      appName: "Random news site generator",
-      alreadyRead: [],
-      toRead:[],
-      selected_article: null,
-    }
-  },
-  components: {
-    //
-  },
-  computed: {
-    readingStatus() {
-      if (this.alreadyRead.length == 0 && this.toRead.length == 0) return "Add something to read to get the show started";
-      if (this.alreadyRead.length > 0 && this.toRead.length == 0) return "Was that it? Add more below"
-      if (this.toRead.length == 0) return "Get reading!"
-      return "hmm - this is strange";
-    }
-  },
-  methods: {
-    // add a new article to the reading list
-    func6() {
-      //include depedencny
-      const axios = require('axios');
-      // create get call to grab list of potential article soruces
-      try {
-        axios.get("/test-data.json").then(response => {
-            // response          
-            let res = response;
-            // data from response
-            let data = res.data;
-            // make random number 
-            let r = Math.max(Math.floor(Math.random() * 5) - 1,0);
-            // get random article
-            let ra = data.results[r];
-            // update the state
-            this.toRead.push(ra);
-        });
-      }
-      catch (error) {
-        alert(error);
-      }
-    
-    },
-    func5(article) {
-      try{
-        return this.selected_article.title == article.title;
-      } catch(exception) {
-        return false;
-      }
-    },
-    func1(article) {
-      return this.selected_article = article;
-    },
-    func2(article) {
-      if (this.selected_article.title == article.title) this.selected_article = null;
-    },
-    func3(ar) {
-      this.alreadyRead.push(ar);
-      let i = this.toRead.findIndex(function(a) {
-        return a.url == ar.url;
+interface ArticleType {
+  title: string,
+  abstract: string,
+  url: string
+}
+
+@Component({})
+
+export default class App extends Vue{
+  appName: string = "Random news site generator";
+  alreadyRead: Array<ArticleType> = [];
+  unreadArticles: Array<ArticleType> = [];
+  selected_article: ArticleType | null = null;
+  
+  get readingStatus(): string {
+    if (this.alreadyRead.length == 0 && this.unreadArticles.length == 0) return "Add something to read to get the show started";
+    if (this.alreadyRead.length > 0 && this.unreadArticles.length == 0) return "Was that it? Add more below"
+    if (this.unreadArticles.length == 0) return "Get reading!"
+    return "hmm - this is strange";
+  }
+  // add a new article to the reading list
+   fetchArticle(): void {
+    //include depedencny
+    const axios = require('axios');
+    // create get call to grab list of potential article soruces
+    try {
+      axios.get("/test-data.json").then( response => {
+          // response          
+          let res = response;
+          // data from response
+          let data = res.data;
+          // make random number 
+          let r = Math.max(Math.floor(Math.random() * 5) - 1,0);
+          // get random article
+          let ra = data.results[r];
+          // update the state
+          this.unreadArticles.push(ra);
       });
-      this.toRead.splice(i,1);
+    }
+    catch (error) {
+      alert(error);
+    }
+
+  }
+
+  func5(article: ArticleType) : boolean{
+    try{
+      return this.selected_article.title == article.title;
+    } catch(exception) {
+      return false;
     }
   }
+
+  showAbstract(article: ArticleType): ArticleType {
+    return this.selected_article = article;
+  }
+
+  markAsRead(article: ArticleType): void {
+    this.alreadyRead.push(article);
+    let i = this.unreadArticles.findIndex(function(a) {
+      return a.url == article.url;
+    });
+    this.unreadArticles.splice(i,1);
+  }
+
 }
+
 </script>
 
